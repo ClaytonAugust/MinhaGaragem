a051cd01582d7230d56ba669430d3641fd92924d
Clayton Augusto Nascimento de Araújo <130181629+ClaytonAugust@users.noreply.github.com>
2023-06-13 18:36:09 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function abrirMenu(){
-    document.getElementById("menuOculto").style.width="230px";
-    document.getElementById("principal").style.marginLeft="250px";
-}
-function fecharMenu(){
-    document.getElementById("menuOculto").style.width="0";
-    document.getElementById("principal").style.marginLeft="0";
-}
